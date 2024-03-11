c6a34eccb7695302de041df68e7997f596498b1f
Rahul Vishwakarma <94642476+imrahulsharmaa@users.noreply.github.com>
2024-03-11 13:09:42 +0530

    Update stat.c

--- fs/stat.c
+++ fs/stat.c
@@ -151,7 +151,7 @@ EXPORT_SYMBOL(vfs_statx_fd);
 #ifdef CONFIG_KSU
 extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
 #endif
-+
+
 /**
  * vfs_statx - Get basic and extra attributes by filename
  * @dfd: A file descriptor representing the base dir for a relative filename
