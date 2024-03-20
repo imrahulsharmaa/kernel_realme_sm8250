b01079d142c0de3d271949f6e7f67803962a37dd
Rahul Vishwakarma <94642476+imrahulsharmaa@users.noreply.github.com>
2024-03-20 20:26:10 +0530

    Update anykernel.sh

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Realme GT Neo 2 / Realme X50 Pro B I G S H O T
+kernel.string=Realme GT Neo 2 | A s t r a L i n k
 do.devicecheck=0
 do.modules=0
 do.systemless=1
