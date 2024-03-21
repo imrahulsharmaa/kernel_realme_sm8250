0a1505b87c9db5b6fc4ca1b12888f80d1474ee97
Rahul Vishwakarma <94642476+imrahulsharmaa@users.noreply.github.com>
2024-03-21 09:37:20 +0530

    Update zip.sh

--- zip.sh
+++ zip.sh
@@ -2,6 +2,6 @@ cp out/arch/arm64/boot/Image ./anykernel/
 cp out/arch/arm64/boot/dtbo.img ./anykernel/
 
 
-cd anykernel && zip -r Bigshot-kernel-AOSP-$(date +"%d-%m-%Y"-%H%M).zip * && mv Bigshot-kernel-AOSP-$(date +"%d-%m-%Y-%H%M").zip ../out/
+cd anykernel && zip -r AstraLink-kernel-AOSP-KSU-$(date +"%d-%m-%Y"-%H%M).zip * && mv AstraLink-kernel-AOSP-KSU-$(date +"%d-%m-%Y-%H%M").zip ../out/
 
 cd ..
